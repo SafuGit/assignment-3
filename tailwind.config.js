5742f61e0870935f1e2d2866a1607bab28c9197d
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-01-31 17:48:30 +0600

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
+      fontFamily: {
+        'sora-font': ['Sora', 'sans-serif']
+      }
     },
     plugins: [],
   }
